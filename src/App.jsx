192709214e51fdd38c200a0fba4a12ef77439c97
mehrdad <mehrdadosoulisaran@gmail.com>
2024-12-31 13:17:54 +0330

    install necessary package in project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="animate__animated animate__bounce">An animated element</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from "react-router";
+import { store } from '../src/redux/app/store.js'
+import { Provider } from 'react-redux'
+import 'animate.css';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+)
