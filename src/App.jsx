6588ab498d8f12b9a6b9dd893e99afb314361674
mehrdad <mehrdadosoulisaran@gmail.com>
2025-01-04 13:07:37 +0330

    set function theme in the utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import AOS from 'aos';
 import { useEffect } from 'react';
-import { theme } from './utils/theme';
+import { theme, loadTheme } from './utils/theme';
 
 function App() {
-  useEffect(() => {  
+  useEffect(() => {
     AOS.init();
   }, []);
-  useEffect(() => {  
-    if (localStorage.getItem('theme') == null) {
-      document.documentElement.classList.add("light");
-      localStorage.setItem("theme", "light");
-    }else{
-      document.documentElement.classList.add(localStorage.getItem('theme'));
-    }
+  useEffect(() => {
+    loadTheme()
   }, []);
   return (
     <>
-    <button onClick={theme}>click theme</button>
-      <div data-aos="fade-up"><button className='text-red-600 dark:text-blue-900'>click to dark</button></div> 
+      <button onClick={theme}>click theme</button>
+      <div data-aos="fade-up"><button className='text-red-600 dark:text-blue-900'>click to dark</button></div>
     </>
   )
 }
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,14 +1,22 @@
-function theme() { 
-  
-     if(localStorage.getItem('theme') == 'light'){
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    }else {
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("light");
-      localStorage.setItem("theme", "light");
-    }
-  
+function theme() {
+  if (localStorage.getItem("theme") == "light") {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+    localStorage.setItem("theme", "light");
+  }
 }
-export { theme };
+
+function loadTheme() {
+  if (localStorage.getItem("theme") == null) {
+    document.documentElement.classList.add("light");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.classList.add(localStorage.getItem("theme"));
+  }
+}
+
+export { theme, loadTheme };
