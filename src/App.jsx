dcb81dd25f504861b0248ae9b86c87131870207a
mehrdad <mehrdadosoulisaran@gmail.com>
2025-02-08 09:15:33 +0330

    set button theme and fixed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import AOS from 'aos';
-import { useEffect, useState } from 'react';
-import { loadTheme, theme } from './utils/theme';
 import Routers from './routes/Route.jsx';
 import HeaderTop from './components/header/HeaderTop.jsx';
 import HeaderMain from './components/header/HeaderMain.jsx';
 import Footer from './components/footer/Footer.jsx';
 
 function App() {
-  const [getTheme, setGetTheme] = useState(localStorage.getItem('theme'))
-  useEffect(() => {
-    AOS.init();
-  }, []);
-  useEffect(() => {
-    loadTheme()
-  }, []);
+
 
   return (
     <div>
-      <button onClick={() => { theme(); setGetTheme(localStorage.getItem('theme')) }}>click</button>
+      
       <HeaderTop />
       <HeaderMain />
       <Routers />
--- src/components/header/HeaderTop.jsx
+++ src/components/header/HeaderTop.jsx
@@ -1,12 +1,32 @@
 import Search from "../search/Search";
-
+import AOS from 'aos';
+import { useEffect, useState } from 'react';
+import { loadTheme, theme } from '../../utils/theme';
+import MoonIcon from "../../../public/photo/MoonIcon";
+import SunIcon from "../../../public/photo/SunIcon";
 
 export default function HeaderTop() {
+  const [getTheme, setGetTheme] = useState(localStorage.getItem('theme'))
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  useEffect(() => {
+    loadTheme()
+  }, []);
+
   return (
     <div className="grid grid-cols-4 items-center px-24 pt-10 gap-5">
       <h1 className="col-span-1 text-xl mt-2 text-primaryColor dark:text-white font-medium">MEHRDAD ABZAR</h1>
       <Search />
       <div className="flex justify-end items-center gap-2 col-span-1">
+        <div className="flex gap-2 justify-center items-center dark:text-white text-black hover:cursor-pointer" onClick={() => { theme(); setGetTheme(localStorage.getItem('theme')) }}>
+          {
+            getTheme == 'light' ? 'تم تیره' : 'تم روشن'
+          }
+          {
+            getTheme == 'light' ? <MoonIcon /> : <SunIcon />
+          }
+        </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 dark:text-white">
           <path d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
         </svg>
@@ -20,7 +40,7 @@ export default function HeaderTop() {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 dark:text-white">
             <path d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
           </svg>
-          <span className="w-4 absolute top-3 right-0 flex justify-center items-center bg-primaryColor text-white leading-4 rounded-full">0</span>
+          <span className="size-4 absolute top-4 right-0 flex justify-center items-center text-base pt-1 bg-primaryColor text-white rounded-full">0</span>
         </div>
       </div>
     </div>
