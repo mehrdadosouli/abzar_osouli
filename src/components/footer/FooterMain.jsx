2c5c3d6d5938ea263a5b7535061f36bfda2094d7
mehrdad <mehrdadosoulisaran@gmail.com>
2025-02-14 08:20:31 +0330

    set responsive for footer

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FooterMain() {
     return (
         <div className='grid grid-cols-4 bg-[#F0F0F0] py-10 px-20'>
-            <div className='grid col-span-1'>
+            <div className='grid md:col-span-1 col-span-2'>
                 <ul className='flex flex-col gap-4'>
                     <li className='font-semibold'>راهنمای مشتریان</li>
                     <li className='text-txt_color_footer_l'>پاسخ به پرسش های متداول</li>
@@ -12,7 +12,7 @@ function FooterMain() {
                     <li className='text-txt_color_footer_l'>قوانین و مقررات</li>
                 </ul>
             </div>
-            <div className='grid col-span-1'>
+            <div className='grid md:col-span-1 col-span-2'>
                 <ul className='flex flex-col gap-4'>
                     <li className='font-semibold'>راهنمای مشتریان</li>
                     <li className='text-txt_color_footer_l'>پاسخ به پرسش های متداول</li>
@@ -21,12 +21,12 @@ function FooterMain() {
                     <li className='text-txt_color_footer_l'>قوانین و مقررات</li>
                 </ul>
             </div>
-            <div className='grid col-span-2 justify-end gap-5'>
+            <div className='md:grid md:col-span-2 md:justify-end md:mt-0 col-span-4 flex justify-between items-center gap-5 mt-10'>
                 <div className='flex justify-between'>
                     <img src="/photo/zarinpall.png" alt="" />
                     <img src="/photo/enamad.png" alt="" />
                 </div>
-                <div className='flex flex-wrap gap-4'>
+                <div className='md:flex md:flex-wrap grid grid-cols-2 gap-4'>
                     <img src="/photo/Group 69.png" alt="" />
                     <img src="/photo/Group 70.png" alt="" />
                     <img src="/photo/Group 71.png" alt="" />
--- src/components/footer/FooterTop.jsx
+++ src/components/footer/FooterTop.jsx
@@ -6,11 +6,11 @@ import FastSend from '../../../public/photo/FastSend'
 
 function FooterTop() {
     return (
-        <div className='grid grid-cols-4 mt-20 p-6 bg-primaryColor [&>*]:text-white'>
-            <span className='border-l-2 flex items-center justify-center gap-5'><Wallet /> امکان پرداخت در محل</span>
-            <span className='border-l-2 flex items-center justify-center gap-5'><Phone /> پشتیبانی ۲۴ ساعته</span>
-            <span className='border-l-2 flex items-center justify-center gap-5'><Garanty /> ضمانت اصلالت کالا</span>
-            <span className='flex items-center justify-center gap-5' ><FastSend /> ارسال سریع</span>
+        <div className='grid grid-cols-4 mt-20 p-6 bg-primaryColor [&>*]:text-white md:text-lg text-sm'>
+            <span className='border-l-2 flex items-center justify-center md:gap-5 gap-2 px-1'><Wallet /> امکان پرداخت در محل</span>
+            <span className='border-l-2 flex items-center justify-center md:gap-5 gap-2 px-1'><Phone /> پشتیبانی ۲۴ ساعته</span>
+            <span className='border-l-2 flex items-center justify-center md:gap-5 gap-2 px-1'><Garanty /> ضمانت اصلالت کالا</span>
+            <span className='flex items-center justify-center md:gap-5 gap-2' ><FastSend /> ارسال سریع</span>
         </div>
     )
 }
